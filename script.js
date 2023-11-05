17f789a5eaa4b0b4a8f84ee503064b9d7fe3d094
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2023-11-05 14:58:46 -0600

    add function to take input from player and play round

--- script.js
+++ script.js
@@ -12,13 +12,67 @@ if 3
 
 function getComputerChoice() {
     let choice = Math.ceil(Math.random() * 3)
-    console.log(choice)
+    console.log(choice) //debug
     if (choice === 1) {
-        return "Rock";
+        return "rock";
     } else if (choice === 2) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
+
+
+
+
+
+/*
+if ps is rock and cs is rock
+    its a tie 
+else if ps is rock and cs is paper
+    cs wins!
+else if ps is rock and cs is scissors
+    ps wins!
+
+if ps is paper and 
+
+I wonder if there's a way to do this without hard coding the selections
+
+
+*/
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "It's a Tie!"
+    } else  if (playerSelection === "rock" && computerSelection == "paper"){
+        return "Computer Wins!"
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win!"
+    }
+
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win!"
+    } else  if (playerSelection === "paper" && computerSelection == "paper"){
+        return "It's a Tie!"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "Computer Wins!"
+    }
+
+    if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "Computer Wins!"
+    } else  if (playerSelection === "scissors" && computerSelection == "paper"){
+        return "You Win!"
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "It's a Tie!"
+    }
+
+
+}
+
+let playerSelection = prompt("Make Your Move").toLowerCase()
+const computerSelection = getComputerChoice()
+
+console.log(playRound(playerSelection, computerSelection))
+
+
+
 
