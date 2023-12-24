c663380f54b17e975612dffed858a806a0cc2b31
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2023-12-24 16:23:12 -0600

    added usable buttons

--- script.js
+++ script.js
@@ -20,57 +20,70 @@ let winner = ""
 
 
 // Plays one round between computer
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "It's a Tie"
-    } 
-    if (playerSelection === "rock" && computerSelection === "scissors" ||
-    playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" &&
-    computerSelection === "rock") {
-        playerWins++
-        return "Player Wins!"
-    } 
-    
-    
-    if (computerSelection === "rock" && playerSelection === "scissors" ||
-    computerSelection === "scissors" && playerSelection === "paper" || computerSelection === "paper" &&
-    playerSelection === "rock") {
-        computerWins++
-        return "Computer Wins!"
-    } 
-    
-
-}
-
-
-
 
 
 
 function game() {
-    for (i = 0; i < 20; i++) {
-    let playerSelection = prompt("Make Your Move").toLowerCase()
+    
+    
     const computerSelection = getComputerChoice()
 
     console.log(playRound(playerSelection, computerSelection))
     console.log("Player Wins: " + playerWins)
     console.log("Computer Wins: " + computerWins)
 
-    if (playerWins == 5 || computerWins == 5) {
-        break
-    }
+    
+    
+    
+}
+
+
+
+window.onload = function() {
+
+    function playRound (playerSelection, computerSelection) {
+        if (playerSelection === computerSelection) {
+            return "It's a Tie"
+        } 
+        if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" &&
+        computerSelection === "rock") {
+            playerWins++
+            return "Player Wins!"
+        } 
+        
+        
+        if (computerSelection === "rock" && playerSelection === "scissors" ||
+        computerSelection === "scissors" && playerSelection === "paper" || computerSelection === "paper" &&
+        playerSelection === "rock") {
+            computerWins++
+            return "Computer Wins!"
+        } 
+        
     
     }
+
+
+const rock = document.querySelector('#rock')
+rock.addEventListener('click', () => {
+    playerSelection = "rock"
+    computerSelection = getComputerChoice()
+    console.log(playRound(playerSelection, computerSelection))
+})
+
+const paper = document.querySelector('#paper')
+paper.addEventListener('click', () => {
+    playerSelection = "paper"
+    computerSelection = getComputerChoice()
+    console.log(playRound(playerSelection, computerSelection))
+})
+
+const scissors = document.querySelector('#scissors')
+scissors.addEventListener('click', () => {
+    playerSelection = "scissors"
+    computerSelection = getComputerChoice()
+    console.log(playRound(playerSelection, computerSelection))
+})
 }
-
-
-game()
-
-if (playerWins > computerWins) {
-    console.log("You beat the computer!")
-} else {
-    console.log("The computer wins.")
-}
-
 
 
